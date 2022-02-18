d86dac26a8d82e37ba1659b0c7ff46ce862a1bc4
Pedro Azevedo <pedro.azvd01@gmail.com>
2022-02-17 21:06:27 -0300

    Aula de var, let e const

---
+++ script.js
@@ -0,0 +1,41 @@
+//var
+
+//var altura = 5;
+//var comprimento = 10;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+
+//let
+
+// let forma = "retangulo";
+// let altura = 5;
+// let comprimento = 10;
+//let area;
+
+// if (altura === 'retangulo'){
+//     area = altura * comprimento;
+// } else {
+//     area = altura * comprimento / 2;
+// }
+
+// console.log(area)
+
+const forma = "retangulo";
+const altura = 6;
+const comprimento = 10;
+let area;
+
+if(forma === "retangulo"){
+    area = altura * comprimento;
+} else {
+    area = altura * comprimento / 2;
+}
+
+console.log(area)
+
+//let quando o valor pode ser alterado futuramente.
+//const não pode ser alterado após ser inicializado.
+
